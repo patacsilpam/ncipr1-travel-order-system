066459e8a3976acc79001bace7837e18541c3548
patacsilpam <pamelampatacsil@gmail.com>
2024-07-08 22:03:14 +0800

    feat: bg-color page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="light" className="bg-[#F8FAFB]">
       <body className={inter.className}>{children}</body>
     </html>
   );
