450aaf61092e852126a3e4d936160b60d2cabf17
patacsilpam <pamelampatacsil@gmail.com>
2024-07-09 22:26:35 +0800

    feat: dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Sidebar from '@/app/components/headers/sidebar'
+
+const DashboardPage = () => {
+  return (
+    <div>
+        <Sidebar/>
+       <div className='absolute top-1/2 left-1/2'>asdfasdf</div>
+    </div>
+  )
+}
+
+export default DashboardPage
