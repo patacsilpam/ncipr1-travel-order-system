2f35e7f8e212a32b6392b9d26956ab7d5a2e6cab
patacsilpam <pamelampatacsil@gmail.com>
2024-07-12 05:16:07 +0800

    feat: responsive sidebar

--- app/components/headers/sidebar.tsx
+++ app/components/headers/sidebar.tsx
@@ -1,26 +1,58 @@
 import Image from "next/image"
 import Link from "next/link"
-import { CircleGauge } from "lucide-react"
-import { Compass } from "lucide-react"
+import { CircleGauge, FilePen,Compass, Users, User, Settings, Building2, Menu } from "lucide-react"
 const sidebar = () => {
   return (
-    <div className='h-screen w-fit bg-[#2B2D37] p-2 text-white'>
-      <div className="flex items-center gap-2">
-          <Image src="/logo.png" height={35} width={35} alt="NCIP LOGO"/>
-          <p className=''>TRAVEL ORDER SYSTEM</p>
+    <div className='h-screen lg:w-[270px] bg-[#191b21] text-white'>
+      <div className="flex lg:items-center sm:justify-center lg:justify-start gap-2">
+          <Image src="/logo.png" height={35} width={35} alt="NCIP LOGO" className="my-2 mx-1"/>
+          <p className='hidden lg:flex text-sm'>Travel Order System</p>
       </div>
-      <div>
-        <ul>
-          <li >
-            <Link href="#" className="flex flex-row">
-              <CircleGauge size={28} strokeWidth={2.25} />
-              <span className="hidden lg:flex">Dasdboard</span>
+      <div className="mt-[30px]">
+      <div className="p-3">
+        <Menu />
+      </div>
+        <ul className="w-full space-y-3">
+          <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
+            <Link href="#" className="flex flex-row gap-4">
+              <CircleGauge />
+              <span className="hidden lg:flex">Dashboard</span>
             </Link>
           </li>
-          <li>
-            <Link href="#"  className="flex flex-row">
-              <Compass size={28} strokeWidth={2.25} />
+          <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
+            <Link href="#"  className="flex flex-row gap-4">
+              <FilePen />
               <span className="hidden lg:flex">Travel Request</span>
+            </Link>
+          </li>
+          <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
+            <Link href="#"  className="flex flex-row gap-4">
+              <Compass/>
+              <span className="hidden lg:flex">Travel Orders</span>
+            </Link>
+          </li>
+          <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
+            <Link href="#"  className="flex flex-row gap-4">
+              <Users />
+              <span className="hidden lg:flex">Employees</span>
+            </Link>
+          </li>
+          <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
+            <Link href="#"  className="flex flex-row gap-4">
+              <Building2/>
+              <span className="hidden lg:flex">Divisions</span>
+            </Link>
+            </li>
+            <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
+            <Link href="#"  className="flex flex-row gap-4">
+              <User/>
+              <span className="hidden lg:flex">Users</span>
+            </Link>
+            </li>
+            <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
+            <Link href="#"  className="flex flex-row gap-4">
+              <Settings />
+              <span className="hidden lg:flex">Settings</span>
             </Link>
           </li>
         </ul>
