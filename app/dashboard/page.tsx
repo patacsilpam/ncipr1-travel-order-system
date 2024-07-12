184bdfa43ca179fdd9f0e28d747fccc0969de312
patacsilpam <pamelampatacsil@gmail.com>
2024-07-13 06:57:58 +0800

    dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ const DashboardPage = () => {
     <div className='flex flex-row h-full'>
         <Sidebar/>
         <div className='w-full bg-slate-400'>
-          sfdadsdf
+          dashboard
         </div>
     </div>
   )
