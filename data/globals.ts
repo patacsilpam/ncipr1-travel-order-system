c9ac918a170af9fd4a031b640f360aa37ea5264d
patacsilpam <pamelampatacsil@gmail.com>
2024-07-13 06:58:20 +0800

    data

---
+++ data/globals.ts
@@ -0,0 +1,45 @@
+const navItems = [
+    {
+        id:1,
+        name: "Dashboard",
+        link:"/dashboard",
+        icon: "<CircleGauge />"
+    },
+    {
+        id:2,
+        name: "Travel Request",
+        link:"/travel-request",
+        icon: "<FilePen />"
+    },
+    {
+        id:3,
+        name: "Travel Orders",
+        link:"/travel-orders",
+        icon: "<Compass />"
+    },
+    {
+        id:4,
+        name: "Employees",
+        link:"/employees",
+        icon: "<Users />"
+    },
+    {
+        id:5,
+        name: "Divisions",
+        link:"/divisions",
+        icon: "<Building2 />"
+    },
+    {
+        id:6,
+        name: "Users",
+        link:"/users",
+        icon: "<User />"
+    },
+    {
+        id:7,
+        name: "Settings",
+        link:"/settings",
+        icon: "<Settings />"
+    }
+    
+]
