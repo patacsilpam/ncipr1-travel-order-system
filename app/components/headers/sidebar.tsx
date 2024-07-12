426b7a5f8b3e9fde0d2fe9f7746d4fa7b52cdc00
patacsilpam <pamelampatacsil@gmail.com>
2024-07-13 06:57:47 +0800

    feat:  responsive sidebar

--- app/components/headers/sidebar.tsx
+++ app/components/headers/sidebar.tsx
@@ -1,58 +1,67 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
 import { CircleGauge, FilePen,Compass, Users, User, Settings, Building2, Menu } from "lucide-react"
-const sidebar = () => {
+import { useState } from "react"
+import { usePathname, useRouter } from "next/navigation"
+const Sidebar = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
+  const pathname = usePathname();
   return (
-    <div className='h-screen lg:w-[270px] bg-[#191b21] text-white'>
+    <div className={`${isMenuVisible ? "w-[50px]" : "lg:w-[270px]"} h-screen bg-[#191b21] text-white`}>
       <div className="flex lg:items-center sm:justify-center lg:justify-start gap-2">
           <Image src="/logo.png" height={35} width={35} alt="NCIP LOGO" className="my-2 mx-1"/>
-          <p className='hidden lg:flex text-sm'>Travel Order System</p>
+          <p className={`${isMenuVisible ? "hidden" : "hidden lg:flex"}`}>Travel Order System</p>
       </div>
       <div className="mt-[30px]">
       <div className="p-3">
-        <Menu />
+        <Menu onClick={toggleMenu} className="hover:bg-[#ffffff2a]" />
       </div>
-        <ul className="w-full space-y-3">
+        <ul className="w-full space-y-3 mt-7">
+          <li className={`hover:bg-slate-200 hover:text-stone-950 p-3 ${pathname === '/dashboard' ? 'bg-slate-200 text-stone-950' : ''}`}>
+            <Link href="/dashboard" className="flex flex-row gap-4">
+              <CircleGauge />
+              <span className={`${isMenuVisible ? "hidden" : "hidden lg:flex"}`}>Dashboard</span>
+            </Link>
+          </li>
+          <li className={`hover:bg-slate-200 hover:text-stone-950 p-3 ${pathname === '/travel-request' ? 'bg-slate-200 text-stone-950' : ''}`}>
+            <Link href="/travel-request" className="flex flex-row gap-4">
+              <FilePen />
+              <span className={`${isMenuVisible ? "hidden" : "hidden lg:flex"}`}>Travel Request</span>
+            </Link>
+          </li>
           <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
             <Link href="#" className="flex flex-row gap-4">
-              <CircleGauge />
-              <span className="hidden lg:flex">Dashboard</span>
+              <Compass />
+              <span className={`${isMenuVisible ? "hidden" : "hidden lg:flex"}`}>Travel Orders</span>
             </Link>
           </li>
           <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
-            <Link href="#"  className="flex flex-row gap-4">
-              <FilePen />
-              <span className="hidden lg:flex">Travel Request</span>
-            </Link>
-          </li>
-          <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
-            <Link href="#"  className="flex flex-row gap-4">
-              <Compass/>
-              <span className="hidden lg:flex">Travel Orders</span>
-            </Link>
-          </li>
-          <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
-            <Link href="#"  className="flex flex-row gap-4">
+            <Link href="#" className="flex flex-row gap-4">
               <Users />
-              <span className="hidden lg:flex">Employees</span>
+              <span className={`${isMenuVisible ? "hidden" : "hidden lg:flex"}`}>Employees</span>
             </Link>
           </li>
           <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
-            <Link href="#"  className="flex flex-row gap-4">
-              <Building2/>
-              <span className="hidden lg:flex">Divisions</span>
+            <Link href="#" className="flex flex-row gap-4">
+              <Building2 />
+              <span className={`${isMenuVisible ? "hidden" : "hidden lg:flex"}`}>Divisions</span>
             </Link>
-            </li>
-            <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
-            <Link href="#"  className="flex flex-row gap-4">
-              <User/>
-              <span className="hidden lg:flex">Users</span>
+          </li>
+          <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
+            <Link href="#" className="flex flex-row gap-4">
+              <User />
+              <span className={`${isMenuVisible ? "hidden" : "hidden lg:flex"}`}>Users</span>
             </Link>
-            </li>
-            <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
-            <Link href="#"  className="flex flex-row gap-4">
+          </li>
+          <li className="hover:bg-slate-200 hover:text-stone-950 p-3">
+            <Link href="#" className="flex flex-row gap-4">
               <Settings />
-              <span className="hidden lg:flex">Settings</span>
+              <span className={`${isMenuVisible ? "hidden" : "hidden lg:flex"}`}>Settings</span>
             </Link>
           </li>
         </ul>
@@ -61,4 +70,4 @@ const sidebar = () => {
   )
 }
 
-export default sidebar
+export default Sidebar
