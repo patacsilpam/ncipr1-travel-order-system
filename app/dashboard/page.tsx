ffa23028cddb32329851ff7cbbd03ce2595b280b
patacsilpam <pamelampatacsil@gmail.com>
2024-07-13 22:08:49 +0800

    dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,22 @@
+"use client"
 import Sidebar from '@/app/components/headers/sidebar'
+import { useState } from 'react'
 
 const DashboardPage = () => {
+  const [isSidebarVisible,setSidebarVisibility] = useState(false);
+  const toggleSidebar = () =>{
+    setSidebarVisibility(!isSidebarVisible);
+  }
   return (
     <div className='flex flex-row h-full'>
         <Sidebar/>
+      
+       
         <div className='w-full bg-slate-400'>
           dashboard
+         
         </div>
+        
     </div>
   )
 }
