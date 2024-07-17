dcbaa9412cd530b6cf367d675b37670c96f837be
patacsilpam <pamelampatacsil@gmail.com>
2024-07-18 06:21:23 +0800

    ui: wizard form

---
+++ app/components/ui/WizardForm.tsx
@@ -0,0 +1,12 @@
+"use client"
+import StepOne from "./StepOne"
+import StepTwo from "./StepTwo"
+import { useState } from "react"
+
+const WizardForm = () => {
+  return (
+    <div>WizardForm</div>
+  )
+}
+
+export default WizardForm
